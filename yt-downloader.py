39f50bbfc36033af05ab1a2f6d99979ee2e579d5
gamanicoercion <140986172+gamanicoercion@users.noreply.github.com>
2023-08-02 22:07:40 +0300

    removed video audio options, didn't work

--- yt-downloader.py
+++ yt-downloader.py
@@ -114,23 +114,10 @@ def main(link_or_search):
         else:
             print(color("Not a viable input!\nClosing...", "91"))
             exit()
-        audio = input(color("choose the audio quality\n(worst, average/avg, best): ", g))
-        if audio in ['best', 'bet', 'bset', 'b']:
-            os.chdir(f"yt-dlp-py/{file_type}")
-            os.system(f'yt-dlp -f bv+ba -o "{inp_name}" -S res:{vid_quality} --recode-video {file_type} "https://www.youtube.com{url}"')
-            done(inp_name)
-        elif audio in ["worst", "Worst", ""]:
-            os.chdir(f"yt-dlp-py/{file_type}")
-            os.system(f'yt-dlp -f bv+wa -o "{inp_name}" -S res:{vid_quality} --recode-video {file_type} "https://www.youtube.com{url}"')
-            done(inp_name)
-        elif audio in ['avg', 'average', 'avg', 'av']:
-            os.chdir(f"yt-dlp-py/{file_type}")
-            os.system(f'yt-dlp -o "audio" -x --audio-format opus --audio-quality 5  -S res:{vid_quality},aext "https://www.youtube.com{url}"')
-            os.system(f'yt-dlp -o "video" --recode-video {file_type} -S res:{vid_quality},aext "https://www.youtube.com{url}')
-            os.system(f'ffmpeg -i video.{file_type} -i audio.opus -map 0:v -map 1:a -c:v copy -c:a copy {inp_name}.{file_type} -y')
-            os.remove("audio.opus")
-            os.remove(f"video.{file_type}")
-            done(inp_name)
+
+        os.chdir(f"yt-dlp-py/{file_type}")
+        os.system(f'yt-dlp -f bv+ba -o "{inp_name}" -S res:{vid_quality} --recode-video {file_type} "https://www.youtube.com{url}"')
+        done(inp_name)
 
 link_or_search = input(color('Do you want to download with a link or through searching YouTube?\n(1 or any input for search/2 or "link" for link) ', a))
 main(link_or_search)
